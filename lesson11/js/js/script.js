8d9fd90245a89a546dcb6b75bc2342e2c7a981bd
VladBezgin <39759704+VladBezgin@users.noreply.github.com>
2018-06-21 13:30:07 +0400

    Update script.js

--- lesson11/js/js/script.js
+++ lesson11/js/js/script.js
@@ -119,39 +119,47 @@ window.addEventListener('DOMContentLoaded', function() {
     message.success = 'Спасибо! Скоро мы с вами свяжемся';
     message.failure = 'Что-то пошло не так...';
 
-    let form = document.getElementsByClassName('main-form')[0],
-    	input = form.getElementsByTagName('input'),
-    	statusMessage = document.createElement('div');
-    	statusMessage.classList.add('status');
+    let form1 = document.getElementsByClassName('main-form')[0],
+        input1 = form1.getElementsByTagName('input'),
+        form2 = document.getElementsByClassName('main-form')[1],
+        input2 = form2.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
 
-    	form.addEventListener('submit', function(event) {
-    		event.preventDefault();
-    		form.appendChild(statusMessage);
+    function formFunction(form, input) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
 
-    		let request = new XMLHttpRequest();
-    		request.open('POST', 'server.php')
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php')
 
-    		request.setRequestHeader('Content-Type' , 'application/x-www-form-urlencoded');
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-    		let formData = new FormData(form);
+            let formData = new FormData(form);
 
-    		request.send(formData);
+            request.send(formData);
 
-    		request.onreadystatechange = function() {
-    			if (request.readyState < 4) {
-    				statusMessage.innerHTML = message.loading;
-    			} else if (request.readyState === 4) {
-    				if (request.status == 200 && request.status < 300) {
-    					statusMessage.innerHTML = message.success;
-    				} else {
-    					statusMessage.innerHTML = message.failure;
-    				}
-    			}
-    		}
-    		for (let i = 0; i < input.length; i++) {
-    			input[i].value = '';
-    		}
-    	});
+            request.onreadystatechange = function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                        statusMessage.innerHTML = message.success;
+                    } else {
+                        statusMessage.innerHTML = message.failure;
+                    }
+                }
+            }
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        });
+    }
+
+    formFunction(form1, input1);
+    formFunction(form2, input2);
+
 
 });
 
